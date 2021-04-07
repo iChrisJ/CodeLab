96b9576cda95b7393a8cf32d421d80bfb5bb02d9
iChrisJ <chrisjiang@outlook.com>
2021-04-07 23:43:47 +0800

    feat: impl leetcode 86, 138

---
+++ CodeRepos/CodeInCS/0086 Partition List.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/// <summary>
+/// LeetCode: 0086 Partition List
+/// </summary>
+namespace CodeInCS._0086_Partition_List
+{
+
+	// Definition for singly-linked list.
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public class Solution
+	{
+		public ListNode Partition(ListNode head, int x)
+		{
+			ListNode lt_x = new ListNode();
+			ListNode lt_cur = lt_x;
+			ListNode ge_x = new ListNode();
+			ListNode ge_cur = ge_x;
+			while (head != null)
+			{
+				ListNode next = head.next;
+				if (head.val < x)
+				{
+					lt_cur.next = head;
+					lt_cur = lt_cur.next;
+				}
+				else // great than or equal to x.
+				{
+					ge_cur.next = head;
+					ge_cur = ge_cur.next;
+				}
+				head.next = null;
+				head = next;
+			}
+
+			if (ge_x == ge_cur)
+				return lt_x.next;
+			else if (lt_x == lt_cur)
+				return ge_x.next;
+
+			// less than x and great than or equal x linked lists are not empty.
+			lt_cur.next = ge_x.next;
+			return lt_x.next;
+		}
+
+		/// <summary>
+		/// Generate a Linked List from an array.
+		/// </summary>
+		private static ListNode CreateLinkedListFromArray(int[] arr)
+		{
+			if (arr == null || arr.Length == 0)
+				return null;
+
+			ListNode preHead = new ListNode();
+			ListNode cur = preHead;
+			foreach (int item in arr)
+			{
+				cur.next = new ListNode(item);
+				cur = cur.next;
+			}
+
+			return preHead.next;
+		}
+
+		//public static void Main()
+		//{
+		//	ListNode head = CreateLinkedListFromArray(new int[] { 1, 4, 3, 2, 5, 2 });
+		//	ListNode res = new Solution().Partition(head, 3);
+		//}
+	}
+}
---
+++ CodeRepos/CodeInCS/0138 Copy List with Random Pointer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// LeetCode: 0138 Copy List with Random Pointer
+/// </summary>
+namespace CodeInCS._0138_Copy_List_with_Random_Pointer
+{
+	// Definition for a Node.
+	public class Node
+	{
+		public int val;
+		public Node next;
+		public Node random;
+
+		public Node(int _val)
+		{
+			val = _val;
+			next = null;
+			random = null;
+		}
+	}
+
+	/// <summary>
+	/// Implment with HashTable.
+	/// </summary>
+	public class Solution
+	{
+		public Node CopyRandomList(Node head)
+		{
+			if (head == null)
+				return null;
+
+			Dictionary<Node, Node> dict = new Dictionary<Node, Node>();
+			Node cur = head;
+			while (cur != null)
+			{
+				dict.Add(cur, new Node(cur.val));
+				cur = cur.next;
+			}
+
+			cur = head;
+			while (cur != null)
+			{
+				dict[cur].next = cur.next == null ? null : dict[cur.next];
+				dict[cur].random = cur.random == null ? null : dict[cur.random];
+				cur = cur.next;
+			}
+			return dict[head];
+		}
+	}
+	/// <summary>
+	/// Duplicate nodes.
+	/// </summary>
+	public class Solution2
+	{
+		public Node CopyRandomList(Node head)
+		{
+			if (head == null)
+				return null;
+
+			Node cur = head;
+			while (cur != null)
+			{
+				Node next = cur.next;
+				cur.next = new Node(cur.val);
+				cur.next.next = next;
+				cur = cur.next.next;
+			}
+
+			cur = head;
+			while (cur != null)
+			{
+				cur.next.random = cur.random != null ? cur.random.next : null;
+				cur = cur.next.next;
+			}
+
+			cur = head;
+			Node copied_head = new Node(-1);
+			Node copied_prev = copied_head;
+			while (cur != null)
+			{
+				Node copied = cur.next;
+				copied_prev.next = copied;
+				cur.next = copied.next;
+				copied.next = null;
+				cur = cur.next;
+				copied_prev = copied_prev.next;
+			}
+			return copied_head.next;
+		}
+	}
+}

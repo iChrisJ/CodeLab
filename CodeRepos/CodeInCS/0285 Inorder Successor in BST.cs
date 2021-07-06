3ee4898d3351c1028e644ac0f2278910c6bf486b
iChrisJ <chrisjiang@outlook.com>
2021-07-06 22:43:20 +0800

    feat: successor node and tree serialization and deserialization

---
+++ CodeRepos/CodeInCS/0285 Inorder Successor in BST.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/// <summary>
+/// LeetCode: 0285 Inorder Successor in BST
+/// </summary>
+namespace CodeInCS._0285_Inorder_Successor_in_BST
+{
+	// Problem: Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+	// Definition for a binary tree node.
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution
+	{
+		// 1) 如果p节点有右子树, p的后继节点就是p右子树最左节点
+		// 2) 如果p节点没有右子树, 沿p节点的父节点向上查找, 如果当前节点不是其父节点的左子节点,继续向上查找
+		//	直到当前节点是其父节点的左节点, 则其父节点就是p节点的后继节点.
+		public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+		{
+			if (root == null || p == null)
+				return null;
+
+			TreeNode next = null;
+			TreeNode cur = root;
+			while (cur != null && cur.val != p.val)
+			{
+				if (cur.val > p.val)
+				{
+					next = cur;
+					cur = cur.left;
+				}
+				else
+					cur = cur.right;
+			}
+
+			if (cur == null)
+				return null;
+
+			if (cur.right == null)
+				return next;
+
+			cur = cur.right;
+			while (cur.left != null)
+				cur = cur.left;
+			return cur;
+		}
+
+	}
+}
---
+++ CodeRepos/CodeInCS/0297 Serialize and Deserialize Binary Tree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// LeetCode: 0297 Serialize and Deserialize Binary Tree
+/// </summary>
+namespace CodeInCS._0297_Serialize_and_Deserialize_Binary_Tree
+{
+	// Definition for a binary tree node.
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public class Codec_PreOrder
+	{
+		// Encodes a tree to a single string.
+		public string serialize(TreeNode root)
+		{
+			StringBuilder tree = new StringBuilder();
+			Serialize(root, tree);
+			return tree.ToString(0, tree.Length - 1);
+		}
+
+		private void Serialize(TreeNode node, StringBuilder tree)
+		{
+			if (node == null)
+			{
+				tree.Append("#,");
+				return;
+			}
+
+			tree.Append(node.val);
+			tree.Append(',');
+			Serialize(node.left, tree);
+			Serialize(node.right, tree);
+		}
+
+		// Decodes your encoded data to tree.
+		public TreeNode deserialize(string data)
+		{
+			string[] vals = data.Split(',');
+			Queue<string> queue = new Queue<string>(vals);
+			return Deserialize(queue);
+		}
+
+		private TreeNode Deserialize(Queue<string> queue)
+		{
+			string val = queue.Dequeue();
+			if (val == "#")
+				return null;
+
+			TreeNode node = new TreeNode(int.Parse(val));
+			node.left = Deserialize(queue);
+			node.right = Deserialize(queue);
+			return node;
+		}
+	}
+}

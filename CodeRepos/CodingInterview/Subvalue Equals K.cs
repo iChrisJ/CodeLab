8079d78de1a0b855b0d0034473054ac7e0a588e2
iChrisJ <chrisjiang@outlook.com>
2021-08-10 23:26:24 +0800

    feat: problems

---
+++ CodeRepos/CodingInterview/Subvalue Equals K.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// [题目]:
+/// 给定一个数组arr，求差值为k的去重数字对
+/// </summary>
+namespace CodingInterview.Subvalue_Equals_K
+{
+	class SubvalueEqualK
+	{
+		public static IList<(int, int)> SubvalueEqualsK(int[] arr, int k)
+		{
+			HashSet<int> set = new HashSet<int>(arr);
+			IList<(int, int)> res = new List<(int, int)>();
+
+			foreach (int cur in set)
+				if (set.Contains(cur + k))
+					res.Add((cur, cur + k));
+			return res;
+		}
+	}
+}
---
+++ CodeRepos/CodingInterview/Unique Binary Trees.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+/// [题目]:
+/// 给定一个非负整数n，代表二叉树的节点个数。返回能形成多少种不同的二叉树结构
+/// </summary>
+namespace CodingInterview.Unique_Binary_Trees
+{
+	class UniqueBinaryTrees
+	{
+		public static int NumTrees(int n)
+		{
+			if (n < 2)
+				return 1;
+
+			int res = 0;
+			for (int i = 0; i <= n - 1; i++)
+			{
+				res += NumTrees(i) * NumTrees(n - 1 - i);
+			}
+			return res;
+		}
+
+		private static int NumTreesDP(int n)
+		{
+			int[] dp = new int[n + 1];
+			dp[0] = 1;
+			for (int i = 1; i < n + 1; i++)
+				for (int j = 0; j < i; j++)
+					dp[i] += dp[j] * dp[i - j - 1];
+
+			return dp[n];
+		}
+
+		//public static void Main()
+		//{
+		//	int n = 10;
+		//	for (int i = 1; i <= n; i++)
+		//	{
+		//		Console.WriteLine($"递归: {NumTrees(i)} - 动态规划: {NumTreesDP(i)}");
+		//	}
+		//	Console.ReadLine();
+		//}
+	}
+}

d0746cf080f797e39a8ef400c868a3f3d3eb46e6
iChrisJ <chrisjiang@outlook.com>
2021-08-16 23:42:45 +0800

    feat: problems practice

---
+++ CodeRepos/CodingInterview/Print Matrix Spiral Order.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/// <summary>
+/// [题目]：
+/// 用螺旋的方式打印矩阵，比如如下的矩阵
+/// 0	1	2	3
+/// 4	5	6	7
+/// 8	9	10	11
+/// 打印顺序为：0 1 2 3 7 11 10 9 8 4 5 6
+/// </summary>
+namespace CodingInterview.Print_Matrix_Spiral_Order
+{
+	class PrintMatrixSpiralOrder
+	{
+		public static void SpiralOrderPrint(int[,] matrix)
+		{
+			int N = matrix.GetLength(0);
+			int M = matrix.GetLength(1);
+
+			for (int x1 = 0, y1 = 0, x2 = N - 1, y2 = M - 1; x1 <= x2 && y1 <= y2; x1++, y1++, x2--, y2--)
+				PrintEdge(matrix, x1, y1, x2, y2);
+		}
+
+		private static void PrintEdge(int[,] matrix, int x1, int y1, int x2, int y2)
+		{
+			if (x1 > x2 || y1 > y2)
+				return;
+
+			for (int i = y1; i < y2; i++)
+				Console.Write($"{matrix[x1, i]} ");
+			for (int i = x1; i < x2; i++)
+				Console.Write($"{matrix[i, y2]} ");
+			for (int i = y2; i > y1; i--)
+				Console.Write($"{matrix[x2, i]} ");
+			for (int i = x2; i > x1; i--)
+				Console.Write($"{matrix[i, y1]} ");
+		}
+
+		//public static void Main()
+		//{
+		//	int[,] matrix = new int[,]
+		//	{
+		//		{ 1, 2, 3, 4 },
+		//		{ 5, 6, 7, 8 },
+		//		{ 9, 10, 11, 12 },
+		//		{ 13, 14, 15, 16 },
+		//		{ 17, 18, 19, 20 }
+		//	};
+
+		//	SpiralOrderPrint(matrix);
+		//}
+	}
+}
---
+++ CodeRepos/CodingInterview/Zig Zag Print Matrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/// <summary>
+/// [题目]:
+/// 用zigzag的方式打印矩阵，比如如下的矩阵
+/// 0	1	2	3
+/// 4	5	6	7
+/// 8	9	10	11
+/// 打印顺序为：0 1 4 8 5 2 3 6 9 10 7 11
+/// </summary>
+namespace CodingInterview.Zig_Zag_Print_Matrix
+{
+	class ZigZagPrintMatrix
+	{
+		// 假设有两个点A,B都是从(0,0)出发，A点向右出发，每次移动一步，到达最右边后向下移动；
+		// B点向下出发，每次移动一步，到达最底端后向右移动。每次移动交替从A->B或者B->A方向
+		// 打印矩阵中的点。
+		public static void PrintMatrixZigZag(int[,] matrix)
+		{
+			int N = matrix.GetLength(0);
+			int M = matrix.GetLength(1);
+			int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+			bool isSlash = false;
+			while (x1 < N && y2 < M)
+			{
+				PrintDiagonal(matrix, x1, y1, x2, y2, isSlash);
+				x1 = y1 == M - 1 ? x1 + 1 : x1;
+				y1 = y1 == M - 1 ? y1 : y1 + 1;
+				y2 = x2 == N - 1 ? y2 + 1 : y2;
+				x2 = x2 == N - 1 ? x2 : x2 + 1;
+				isSlash = !isSlash;
+			}
+		}
+
+		private static void PrintDiagonal(int[,] matrix, int x1, int y1, int x2, int y2, bool isSlash)
+		{
+			if (isSlash)
+			{
+				for (int i = x1, j = y1; i <= x2 && j >= y2; i++, j--)
+					Console.Write($"{matrix[i, j]}\t");
+			}
+			else
+			{
+				for (int i = x2, j = y2; i >= x1 && j <= y1; i--, j++)
+					Console.Write($"{matrix[i, j]}\t");
+			}
+		}
+
+		//public static void Main()
+		//{
+		//	int[,] matrix =
+		//	{
+		//		{ 1, 2, 3, 4 },
+		//		{ 5, 6, 7, 8 },
+		//		{ 9, 10, 11, 12 }
+		//	};
+		//	PrintMatrixZigZag(matrix);
+		//}
+	}
+
+}
